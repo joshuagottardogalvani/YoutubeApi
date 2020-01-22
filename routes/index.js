b0597c9f79ad391f0f4a46eb16e13b3b632792ac
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2020-01-22 22:29:30 +0000

    completo

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,13 @@ router.get('/', function(req, res, next) {
 
 router.post('/search', function(req, res, next) {
 
-    youtube.searchVideos(req.body.search, 10)
+    /* youtube.searchVideos(req.body.search, 10)
     .then(results => {
         res.render('video', { results, searchKey: req.body.search });
     })
-    .catch(console.log);
+    .catch(console.log); */
 
-    /* res.render('video', { results: videoJson.videos, searchKey: req.body.search }); */
+    res.render('video', { results: videoJson.videos, searchKey: req.body.search });
 
 });
 
